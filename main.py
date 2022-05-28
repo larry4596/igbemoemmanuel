cf7eef1e852dfd0d24460599d69981d6972451ce
Emmanuel <50343622+larry4596@users.noreply.github.com>
2022-05-28 15:55:43 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# function to check if two strings are
+# anagram or not
+def check(s1, s2):
+	
+	# the sorted strings are checked
+	if(sorted(s1)== sorted(s2)):
+          (sorted(s3)== sorted(s4)
+		print("The strings are anagrams.")
+	else:
+		print("The strings aren't anagrams.")		
+		
+# driver code
+s1 ="dad"
+s2 ="add"
+check(s1, s2)
+        
+ # driver code
+s3 ="lent"
+s4 ="tent"
+check(s3, s4)
